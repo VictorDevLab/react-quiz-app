56edc2ecc2b1b36e758d2aada0a493c5419efe2a
Victor Koome <koomev786@gmail.com>
2024-07-03 23:32:27 +0300

    add main component composition with children

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import Header from "./Header";
+import Main from "./Main";
 export default function App() {
   return (
     <div className="app">
       <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Questions</p>
+      </Main>
     </div>
   );
 }
