5fa102e596abcd5f760a9bc8ec545f4a2ed19751
Victor Koome <koomev786@gmail.com>
2024-07-04 23:20:59 +0300

    fetch the json data from server

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { useReducer, useEffect } from "react";
+
 import Header from "./Header";
 import Main from "./Main";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: {} }))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="app">
       <Header />
