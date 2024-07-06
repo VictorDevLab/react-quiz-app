c716140138aee08623b5e713dc67a315956bcdb7
Victor Koome <koomev786@gmail.com>
2024-07-06 15:31:37 +0300

    separate components

---
+++ src/components/Options.js
@@ -0,0 +1,9 @@
+function Options({question}) {
+    return (
+        <div className="options">
+        {question.options.map(option => <button className="btn btn-option">{option}</button>)}
+     </div>
+    )
+}
+
+export default Options
